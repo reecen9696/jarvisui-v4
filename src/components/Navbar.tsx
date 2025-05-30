dbdf4c1efdcfa78865f41a870df211b4cfddb5d1
reecen9696 <reece@cryptocoders.com.au>
2025-05-31 09:41:26 +1000

    jarvis

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -21,7 +21,7 @@ const Navbar = () => {
         {/* Logo */}
         <div className="flex-shrink-0">
           <Link to="/">
-            <Logo className="h-6 w-auto logo-spin" />
+            <Logo className="h-6 w-auto" />
           </Link>
         </div>
 
