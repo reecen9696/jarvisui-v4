76ad1d9b648e0cba4cdb484485c2cdec135910a1
reecen9696 <reece@cryptocoders.com.au>
2025-05-30 17:43:01 +1000

    fixed mobile

--- src/components/sections/SectionOne.tsx
+++ src/components/sections/SectionOne.tsx
@@ -5,10 +5,19 @@ import GlobeAnimation from "../GlobeAnimation";
 const SectionOne: React.FC = () => {
   return (
     <div className="snap-section">
-      <div className="flex flex-col justify-end h-full p-4 md:p-8 jusify-center ">
-        <GlobeAnimation />
-        <div className="w-full mb-28 md:mb-0 md:w-1/3 md:mx-auto">
-          <ChatBox />
+      <div className="relative h-full bg-white p-4 md:p-8">
+        {/* Globe centered on full screen */}
+        <div className="absolute inset-0 flex items-center justify-center">
+          <div className="aspect-square">
+            <GlobeAnimation className="w-full h-full" />
+          </div>
+        </div>
+
+        {/* ChatBox positioned at bottom */}
+        <div className="absolute bottom-24 md:bottom-8 left-4 right-4 md:left-8 md:right-8">
+          <div className="w-full md:w-1/3 md:mx-auto">
+            <ChatBox />
+          </div>
         </div>
       </div>
     </div>
