39990627bbfe932f08cf9e654b905890d8e42d99
reecen9696 <reece@cryptocoders.com.au>
2025-05-30 17:51:02 +1000

    updated jarvis

--- src/components/PageLayout.tsx
+++ src/components/PageLayout.tsx
@@ -13,11 +13,13 @@ const PageLayout: React.FC<PageLayoutProps> = ({
   className = "",
 }) => {
   return (
-    <div className={`min-h-screen bg-white ${className}`}>
-      <div className="max-w-4xl mx-auto p-4 md:p-8 mt-20 pb-32">{children}</div>
-      <div className="mt-12">
-        <BottomNavbar />
-      </div>
+    <div className={`min-h-screen bg-white flex flex-col ${className}`}>
+      <div className="flex-1 max-w-4xl mx-auto p-4 md:p-8 mt-20 pb-8">{children}</div>
+      {showBottomNavbar && (
+        <div className="mt-auto">
+          <BottomNavbar />
+        </div>
+      )}
     </div>
   );
 };
--- src/components/sections/SectionOne.tsx
+++ src/components/sections/SectionOne.tsx
@@ -6,14 +6,12 @@ const SectionOne: React.FC = () => {
   return (
     <div className="snap-section">
       <div className="relative h-full bg-white p-4 md:p-8">
-        {/* Globe centered on full screen */}
         <div className="absolute inset-0 flex items-center justify-center">
           <div className="aspect-square">
             <GlobeAnimation className="w-full h-full" />
           </div>
         </div>
 
-        {/* ChatBox positioned at bottom */}
         <div className="absolute bottom-24 md:bottom-8 left-4 right-4 md:left-8 md:right-8">
           <div className="w-full md:w-1/3 md:mx-auto">
             <ChatBox />
