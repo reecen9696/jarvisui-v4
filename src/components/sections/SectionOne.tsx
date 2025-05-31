b0e808a7e7c07662c169f0ad7a74fe4041fa0dfb
reecen9696 <reece@cryptocoders.com.au>
2025-05-31 10:18:01 +1000

    added scroll

--- src/components/sections/SectionOne.tsx
+++ src/components/sections/SectionOne.tsx
@@ -1,14 +1,23 @@
 import React from "react";
 import ChatBox from "../ChatBox";
 import GlobeAnimation from "../GlobeAnimation";
+import Lottie from "lottie-react";
+import scrollAnimation from "../../assets/lottie/scroll.json";
 
 const SectionOne: React.FC = () => {
   return (
     <div className="snap-section">
       <div className="flex flex-col justify-end h-full p-4 md:p-8 jusify-center ">
         <GlobeAnimation />
-        <div className="w-full mb-6 md:mb-0 md:w-1/3 md:mx-auto">
+        <div className="w-full md:mb-0 md:w-1/3 md:mx-auto">
           <ChatBox />
+          <div className="w-8 h-8 mx-auto mt-4">
+            <Lottie
+              animationData={scrollAnimation}
+              loop={true}
+              autoplay={true}
+            />
+          </div>
         </div>
       </div>
     </div>
